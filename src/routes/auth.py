334a1be4221f07753a39b5b920622235722a5f46
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-06-22 15:49:15 -0500

    Fix call services create player

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 from src.models.auth_model import AuthModel
 from src.models.player_model import PlayerModel
 from src.services.auth_serv import get_auth_player_serv
+from src.services.player_serv import create_player_serv
 from src.utils.jwt_handle import generate_token
 from src.utils.error_handle import get_details_error
 from src.utils.handle_response import send_success_response
@@ -46,13 +47,13 @@ def login_player_ctrl(data: AuthModel):
 @auth_router.post("/register")
 def create_player_ctrl(data: PlayerModel):
     try:
-        player = create_player_ctrl(data)
+        player = create_player_serv(data)
         print(f"Usuario creado: {player.email}")
         return send_success_response(201, "Player created successfully")
     except Exception as error:
         return get_details_error(error)
     
-    
+
 @auth_router.get("/logout")
 def logout_player_ctrl():
         return send_success_response(200, "Logout successful")
