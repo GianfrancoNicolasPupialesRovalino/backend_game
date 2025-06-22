3bb19eefc7b9eaa2e21f46061a9843d98993b3f8
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-06-22 15:48:49 -0500

    Fix import bcrypt

--- src/utils/bcrypt_handle.py
+++ src/utils/bcrypt_handle.py
@@ -1,11 +1,11 @@
-from bcrypt import check_pw, gensalt, hash_pw
+from bcrypt import checkpw, gensalt, hashpw
 
 # Funcion para cifrar la contraseña
 def encrypt(password: str) -> str:
-    password_hashed = hash_pw(password.encode('utf-8'), gensalt())
+    password_hashed = hashpw(password.encode('utf-8'), gensalt())
     return password_hashed.decode('utf-8')
 
 # Funcion para verificar si la contraseña es correcta
 def verified(password: str, password_hash: str) -> bool:
-    is_valid = check_pw(password.encode('utf-8'), password_hash.encode('utf-8'))
+    is_valid = checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
     return is_valid
