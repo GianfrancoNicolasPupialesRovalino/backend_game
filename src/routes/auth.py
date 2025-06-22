18fe6c1b80ee56373ca35a0cd3e65b7a3cb6c267
AnthonyMorales <anthony.morales03@epn.edu.ec>
2025-06-22 13:45:35 -0500

    Add routes auth and player

---
+++ src/routes/auth.py
@@ -0,0 +1,58 @@
+from datetime import timedelta
+
+from fastapi import APIRouter
+
+from src.models.auth_model import AuthModel
+from src.models.player_model import PlayerModel
+from src.services.auth_serv import get_auth_player_serv
+from src.utils.jwt_handle import generate_token
+from src.utils.error_handle import get_details_error
+from src.utils.handle_response import send_success_response
+from src.utils.bcrypt_handle import verified
+
+
+auth_router = APIRouter()
+
+
+@auth_router.post("/login")
+def login_player_ctrl(data: AuthModel):
+    try: 
+        
+        player = get_auth_player_serv(data.email)
+        if not player:
+            return send_success_response(404, "Player not found")
+        if not verified(data.password, player["password"]):
+            return send_success_response(401, "Invalid credentials")
+
+        token = generate_token(player, expires_in=timedelta(minutes=960))
+
+
+        return send_success_response(
+            200,
+            "Login user",
+            {
+                "token": token,
+                "player": {
+                    "email": player.get("email"),
+                    "name": player.get("name"),
+                    "lastname": player.get("lastname"),
+                },
+            },
+        )
+    except Exception as error:    
+        return get_details_error(error)
+    
+
+@auth_router.post("/register")
+def create_player_ctrl(data: PlayerModel):
+    try:
+        player = create_player_ctrl(data)
+        print(f"Usuario creado: {player.email}")
+        return send_success_response(201, "Player created successfully")
+    except Exception as error:
+        return get_details_error(error)
+    
+    
+@auth_router.get("/logout")
+def logout_player_ctrl():
+        return send_success_response(200, "Logout successful")
---
+++ src/routes/player.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+
+
+from src.services.player_serv import get_players_serv, get_player_by_email_serv
+from src.utils.error_handle import get_details_error
+from src.utils.handle_response import send_success_response
+
+player_router = APIRouter()
+
+@player_router.get("/all")
+def get_players_ctrl():
+    try:
+        players = get_players_serv()
+
+        if not players:
+            return send_success_response(404, "No players found")
+        
+        return send_success_response(200, "Players retrieved successfully", players)
+    except Exception as error:
+        return get_details_error(error)
+    
+
+@player_router.get("/{email}")
+def get_player_by_email_ctrl(email: str):
+    try:
+        player = get_player_by_email_serv(email=email)
+
+        if not player:
+            return send_success_response(404, "Player not found")
+        
+        return send_success_response(200, "Player found", player)
+    except Exception as error:
+        return get_details_error(error)
